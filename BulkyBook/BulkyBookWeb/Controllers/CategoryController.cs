e129d612832b5c05867c93d99af66c057b2abda3
Amir-Mohammadi <fskyfall47@gmail.com>
2022-01-21 12:20:06 +0330

    add edit

--- BulkyBook/BulkyBookWeb/Controllers/CategoryController.cs
+++ BulkyBook/BulkyBookWeb/Controllers/CategoryController.cs
@@ -24,5 +24,61 @@ namespace BulkyBookWeb.Controllers
            
             return View();
         }
+
+        //post
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            if(obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("CustomError", "The DisplayOrderCannot exatly Match The Name");
+            }
+            if (ModelState.IsValid)
+            {
+
+            _db.Categories.Add(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+        //Get
+        public IActionResult Edit(int? id)
+        {
+            if(id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var categoryFormDb = _db.Categories.Find(id);
+            //var categoryFormDbFirst = _db.Categories.FirstOrDefault(u => u.Id == id);
+            //var categoryFormDbSingle = _db.Categories.SingleOrDefault(u => u.Id == id);
+            if (categoryFormDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(categoryFormDb);
+        }
+
+        //post
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("CustomError", "The DisplayOrderCannot exatly Match The Name");
+            }
+            if (ModelState.IsValid)
+            {
+
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
     }
 }
